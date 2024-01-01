a1088b37fa4eab2d70f88572a8cc5293eac0ebcd
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-01-01 13:05:05 -0300

    fix: tests

--- tests/UrlShortener.WebApiTests/Controllers/PingControllerTests.cs
+++ tests/UrlShortener.WebApiTests/Controllers/PingControllerTests.cs
@@ -7,13 +7,10 @@ public class PingControllerTests
     [Fact]
     public void Root_ReturnsPong()
     {
-        // Arrange
         var controller = new PingController();
-
-        // Act
+        
         var result = controller.Root();
-
-        // Assert
+        
         Assert.Equal("pong", result);
     }
 }
